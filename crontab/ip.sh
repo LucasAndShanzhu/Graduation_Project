f7d8dc6e5a6d7cc85ecd311844a8c0125cced129
shanzhu <shanzhu@lingyongqian.cn>
2018-03-23 09:34:56 +0800

    ip.sh

--- crontab/ip.sh
+++ crontab/ip.sh
@@ -3,7 +3,7 @@
 index=0
 while true
 do 
-    cd /home/lucas/Graduation_Project/infoflow/infoflow/spiders && python proxydbIp.py
+    # cd /home/lucas/Graduation_Project/infoflow/infoflow/spiders && python proxydbIp.py
     if [ $index == 10 ]
     then 
 	   cd /home/lucas/Graduation_Project/infoflow && python run.py crawl foreignip
