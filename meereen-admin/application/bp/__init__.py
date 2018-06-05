fcaf49f695a86680a5bc83ffeb81198917309dfd
LucasAndShanzhu <lucasandshanzhu@yeah.net>
2018-06-05 16:43:34 +0800

    record

--- meereen-admin/application/bp/__init__.py
+++ meereen-admin/application/bp/__init__.py
@@ -4,9 +4,10 @@
 # @Last Modified by:   shanzhu
 # @Last Modified time: 2018-02-09 10:01:56
 
-from . import loginBp, userRegBp
+from . import loginBp, userRegBp, actionReocrdBp
 
 def registerBp(app):
     app.register_blueprint(loginBp.loginBp)
     app.register_blueprint(userRegBp.userRegBp)
+    app.register_blueprint(actionReocrdBp.actionReocrdBp)
 
